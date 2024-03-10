cb26aa3400aa4a80227a4bc60b04f1a01d67bc04
naiomi <Naiomi.Naidoo@icloud.com>
2024-03-10 12:33:25 +0100

    Initial commit

---
+++ src/main/java/org/naiomi/kafkatestlibary/KafkaTestLibaryApplication.java
@@ -0,0 +1,13 @@
+package org.naiomi.kafkatestlibary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaTestLibaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaTestLibaryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/naiomi/kafkatestlibary/KafkaTestLibaryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.naiomi.kafkatestlibary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaTestLibaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
